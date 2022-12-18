4093d5b879528d9e40d091cfc640fa54bf233f92
lpm0073 <lpm0073@gmail.com>
2022-12-18 14:03:03 -0600

    set hook ids in the dom

--- reactjs/hello-world-1/src/index.js
+++ reactjs/hello-world-1/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('reactjs-hook-hello-world-1'));
 root.render(
   <React.StrictMode>
     <App />
