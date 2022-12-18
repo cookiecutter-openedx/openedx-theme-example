de2627a7c79aeb09118f2f2806a6480b3079216a
lpm0073 <lpm0073@gmail.com>
2022-12-18 15:07:08 -0600

    lint

--- reactjs/hello-world-2/src/App.js
+++ reactjs/hello-world-2/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>edx-theme-example/reactjs/hello-world-2/src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World 2
         </a>
       </header>
     </div>
